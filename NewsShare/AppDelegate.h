1f35d70c02c9209b2f1e39f5bbbc1730114e3327
zhaofeng <zhaofengneu@gmail.com>
2013-06-19 10:39:47 +0800

    Initial Commit

---
+++ NewsShare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NewsShare
+//
+//  Created by 赵 峰 on 13-6-19.
+//  Copyright (c) 2013年 赵 峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NewsShare/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NewsShare
+//
+//  Created by 赵 峰 on 13-6-19.
+//  Copyright (c) 2013年 赵 峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
